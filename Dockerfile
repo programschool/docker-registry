5be6e982f4064fbd3e11ed8256378fa76ed45e3a
foxsir <838394225@qq.com>
2021-05-08 18:13:07 +0800

    update

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,11 @@
 FROM ubuntu:20.04
 
+RUN rm -f /etc/localtime
+RUN ln -s /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+
 COPY registry /home
 COPY config.json /home
+RUN mkdir /home/logs
 WORKDIR /home
 
 # CMD ["java", "-jar", "backend-0.0.1.jar", "--spring.profiles.active=online"]
--- main.go
+++ main.go
@@ -5,11 +5,13 @@ import (
 	"./router"
 	"fmt"
 	"github.com/labstack/echo"
+	"os"
 )
 
 func main() {
 	// Echo instance
 	e := echo.New()
+	initLog(e)
 	// Routes
 	router.Public(e)
 	router.Private(e)
@@ -18,4 +20,14 @@ func main() {
 	fmt.Println("Private Registry Service For boxlayer.com")
 	address := fmt.Sprintf("%s:%s", conf.Host, conf.Port)
 	e.Logger.Fatal(e.StartTLS(address, conf.CertFile, conf.KeyFile))
+}
+
+func initLog(e *echo.Echo) {
+	logFile, err := os.OpenFile("/home/logs/docker-registry.log", os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		panic(err)
+	}
+
+	// 设置存储位置
+	e.Logger.SetOutput(logFile)
 }
